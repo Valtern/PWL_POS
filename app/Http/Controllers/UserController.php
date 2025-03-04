203471eec65c720daecc9f6ad661c0cbe4c81788
Ridho Anfa'al <ridhoanfaalkuliah@gmail.com>
2025-03-04 10:52:58 +0700

    Jb4 practicum-2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,10 +9,20 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        $user = UserModel::where('level_id', 2)->count();
+        dd($user);
         return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOrFail(1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
 
         // $user = UserModel::firstWhere('level_id', 1);
         // return view('user', ['data' => $user]);
