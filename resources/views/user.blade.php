92d2e6bcb119abd7c139211ca96382c129483645
Ridho Anfa'al <ridhoanfaalkuliah@gmail.com>
2025-03-04 11:48:08 +0700

    Jb4 practicum 2.4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,13 +9,14 @@
 <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
+        {{-- <tr>
             <th>Jumlah Pengguna</th>
         </tr>
         <tr>
             <td>{{ $data }}</td>
-        </tr>
-        {{-- <tr>
+        </tr> --}}
+
+        <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
@@ -26,7 +27,7 @@
             <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
-        </tr> --}}
+        </tr>
         {{-- @foreach ($data as $d)
             <tr>
                 <td>{{ $d->user_id }}</td>
