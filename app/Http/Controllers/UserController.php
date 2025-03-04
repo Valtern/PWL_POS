2b4a7738429ea6429ce766290db583df1f425f0a
Antonius Kaharap Kautsar <antonisback123@gmail.com>
2025-03-04 11:04:57 +0700

    burn

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,6 @@ class UserController extends Controller
        //$user = UserModel::where('username', 'manager9')->firstOrFail();
 
        $user = UserModel::where('level_id', 2)->count();
-       dd($user);
         return view('user', ['data' => $user]);
 
 
