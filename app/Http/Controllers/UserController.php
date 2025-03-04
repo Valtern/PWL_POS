0bada4feff6d07f4924bd6bfd37b6f280b181d69
Ridho Anfa'al <ridhoanfaalkuliah@gmail.com>
2025-03-04 11:08:56 +0700

    Jb4 practicum 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index(){
         $user = UserModel::where('level_id', 2)->count();
-        dd($user);
+        //dd($user);
         return view('user', ['data' => $user]);
 
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
