e6b8fc63706742c78c3ea3127b11df3369116b8f
Ridho Anfa'al <ridhoanfaalkuliah@gmail.com>
2025-03-04 10:43:35 +0700

    Jb4 practicum-2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,29 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
 
         // $data = [
         //     'nama' => 'Pelanggan pertama',
