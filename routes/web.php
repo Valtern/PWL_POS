4029d29d0aa90e277c25e77fe2add0b8f6d6a448
Antonius Kaharap Kautsar <antonisback123@gmail.com>
2025-03-17 08:48:39 +0700

    yeah idk

--- routes/web.php
+++ routes/web.php
@@ -1,70 +1,17 @@
 <?php
 
-use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\WelcomeController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// JOB SHEET 2 //////////////////////////////////////////////////////////
-//home page
-// Route::get('/', [HomeController::class, 'index']);
-
-// //category/product
-// Route::prefix('/category')->group(function () {
-//     Route::get('/food-beverage', [ProductsController::class, 'foodBeverage']);
-//     Route::get('/beauty-health', [ProductsController::class, 'beautyHealth']);
-//     Route::get('/home-care', [ProductsController::class, 'homeCare']);
-//     Route::get('/baby-kid', [ProductsController::class, 'babyKid']);
-// });
-
-// //user profile
-// Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
-
-// //sales tx
-// Route::get('/sales', [SalesController::class, 'index']);
-// // END OF JOB SHEET 2/////////////////////////////////////////////////////////////////////
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BarangController;
 
 
-// // JOB SHEET 3 ///////////////////////////////////////////////////////////
-// Route::get('/level', [LevelController::class, 'index']);
-
-// Route::get('/kategori', [KategoriController::class, 'index']);
-
-// Route::get('/user', [UserController::class, 'index']);
-// // END OF JOB SHEET 3/////////////////////////////////////////////////////////////////////
-
-// // JOB SHEET 4 ////////////////////////////////////////////////////////////
-// Route::get('/user/tambah', [UserController::class, 'tambah']);
-
-// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
-// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
-// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
-// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-// // END OF JOB SHEET 4 /////////////////////////////////////////////////////////////////////
-
-// JOB SHEET 5 /////////////////////////////////////////////////////////////
 Route::get('/', [WelcomeController::class, 'index']);
 
 Route::group(['prefix' => 'user'], function() {
@@ -76,5 +23,10 @@ Route::group(['prefix' => 'user'], function() {
     Route::get('/{id}/edit', [UserController::class, 'edit']);
     Route::put('/{id}', [UserController::class, 'update']);
     Route::delete('/{id}', [UserController::class, 'destroy']);
-    
+
 });
+
+Route::resource('level', LevelController::class);
+Route::resource('kategori', KategoriController::class);
+Route::resource('supplier', SupplierController::class);
+Route::resource('barang', BarangController::class);
