b06e11024413f789f8c197631c371db94f1c11ff
Antonius Kaharap Kautsar <antonisback123@gmail.com>
2025-05-06 07:42:37 +0700

    updatw 2

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,8 @@ use App\Http\Controllers\Api\BarangController;
 // });
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login',App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return response()->user();
+});
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
